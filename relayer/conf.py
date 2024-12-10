b39de834d77871e9019bff9720b2fa0ad424f611
Artem B <hoongun@gmail.com>
2024-12-10 19:23:34 +0300

    fixed idle timeout

--- relayer/conf.py
+++ relayer/conf.py
@@ -7,7 +7,7 @@ load_dotenv(SECRETS_FILE or '.env_relayer')
 
 IMAP_HOST = os.environ.get('IMAP_HOST')
 IMAP_PORT = os.environ.get('IMAP_PORT')
-IMAP_IDLE_TIMEOUT = os.environ.get('IMAP_IDLE_TIMEOUT')
+IMAP_IDLE_TIMEOUT = int(os.environ.get('IMAP_IDLE_TIMEOUT'))
 RELAYER_EMAIL = os.environ.get('RELAYER_EMAIL')
 
 GMAIL_REFRESH_TOKEN = os.environ.get('GMAIL_REFRESH_TOKEN')
